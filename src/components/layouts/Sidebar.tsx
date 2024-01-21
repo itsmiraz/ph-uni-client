432c1a05a4308fe48f8d8bbf33114012772730a1
itsmiraz <mirajulislamofficial044@gmail.com>
2024-01-21 10:57:31 +0600

    new contributions

--- src/components/layouts/Sidebar.tsx
+++ src/components/layouts/Sidebar.tsx
@@ -1,10 +1,37 @@
 import { Layout, Menu } from "antd";
 import { SideBarItemsGenerator } from "../../utils/sideBarItemsGenerator";
 import { adminPaths } from "../../routes/admin.routes";
+import { facultyPaths } from "../../routes/faculty.routes";
+import { studentPaths } from "../../routes/student.routes";
 
 const { Sider } = Layout;
 
+const userRole = {
+  ADMIN: "admin",
+  FACULTY: "faculty",
+  STUDENT: "student",
+};
+
 const Sidebar = () => {
+  const currentUserRole = "faculty";
+
+  let sidebarItems;
+
+  switch (currentUserRole) {
+    case userRole.ADMIN:
+      sidebarItems = SideBarItemsGenerator(adminPaths, userRole.ADMIN);
+      break;
+    case userRole.FACULTY:
+      sidebarItems = SideBarItemsGenerator(facultyPaths, userRole.FACULTY);
+      break;
+    case userRole.STUDENT:
+      sidebarItems = SideBarItemsGenerator(studentPaths, userRole.STUDENT);
+      break;
+
+    default:
+      break;
+  }
+
   return (
     <Sider
       breakpoint="lg"
@@ -24,7 +51,7 @@ const Sidebar = () => {
         theme="dark"
         mode="inline"
         defaultSelectedKeys={["4"]}
-        items={SideBarItemsGenerator(adminPaths, "admin")}
+        items={sidebarItems}
       />
     </Sider>
   );
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import { adminPaths } from "./admin.routes";
 import { RouteGeneretor } from "../utils/routeGeneretor";
+import { studentPaths } from "./student.routes";
+import { facultyPaths } from "./faculty.routes";
 
 export const router = createBrowserRouter([
   {
@@ -12,5 +14,15 @@ export const router = createBrowserRouter([
     path: "/admin",
     element: <App />,
     children: RouteGeneretor(adminPaths),
+  },
+  {
+    path: "/student",
+    element: <App />,
+    children: RouteGeneretor(studentPaths),
+  },
+  {
+    path: "/faculty",
+    element: <App />,
+    children: RouteGeneretor(facultyPaths),
   },
 ]);
